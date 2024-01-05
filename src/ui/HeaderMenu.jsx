d0e2c27800af0ef953241192514d6d6530181fd6
Jovan Ng <edtokens@gmail.com>
2024-01-05 13:54:01 +0800

    Modified UI

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -4,17 +4,27 @@ import ButtonIcon from "./ButtonIcon";
 import { HiOutlineUser } from "react-icons/hi2";
 import { useNavigate } from "react-router-dom";
 import DarkModeToggle from "./DarkModeToggle";
+import { useUser } from "../features/authentication/useUser";
 
 const StyledHeaderMenu = styled.ul`
   display: flex;
   gap: 0.4rem;
 `;
 
+const Greetings = styled.li`
+  margin-top: 0.5rem;
+  font-weight: bold;
+  padding-right: 1rem;
+`;
+
 function HeaderMenu() {
   const navigate = useNavigate();
+  const { user } = useUser();
+  const username = user.user_metadata.fullName;
 
   return (
     <StyledHeaderMenu>
+      <Greetings>Welcome back, {username}</Greetings>
       <li>
         <ButtonIcon onClick={() => navigate("/settings")}>
           <HiOutlineUser />
